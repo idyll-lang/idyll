c3fc0cac18e7efc7a42d93a851e8fcf1d9fce2a1
Matthew Conlen <mc@mathisonian.com>
2017-02-16 23:36:11 -0800

    add scroll example

---
+++ examples/scroll/components/chart.js
@@ -0,0 +1,33 @@
+const React = require('react');
+const IdyllComponent = require('idyll-component');
+const V = require('victory');
+
+const types = {
+  LINE: V.VictoryLine,
+  BAR: V.VictoryBar,
+  SCATTER: V.VictoryScatter
+};
+
+class Chart extends IdyllComponent {
+  render() {
+    const scale = 1 / (( this.props.filterIndex % 3  +  1));
+    const INNER_CHART = types[this.props.type.toUpperCase()];
+    const data = Array(100).fill().map((_) => {
+      return {
+        x: scale * Math.random(),
+        y: scale * Math.random()
+      }
+    });
+
+    return (
+      <div className={this.props.className}>
+        <V.VictoryChart domainPadding={10} animate={{ duration: 750, easing: "quadInOut" }}>
+          <INNER_CHART data={data} domain={[0, 1]} >
+          </INNER_CHART>
+        </V.VictoryChart>
+      </div>
+    );
+  }
+}
+
+module.exports = Chart;
---
+++ examples/scroll/components/waypoint.js
@@ -0,0 +1,16 @@
+const React = require('react');
+const IdyllComponent = require('idyll-component');
+
+class Waypoint extends IdyllComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div/>
+    );
+  }
+}
+
+module.exports = Waypoint;
