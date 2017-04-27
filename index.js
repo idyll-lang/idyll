24f9635dd0bcff459df0d4e774b6e9736dd20713
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-04-27 14:27:17 -0400

    Fix typo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var parse = require('./src/parser');
 var Lexer = require('./src/lexer');
 
 module.exports = function(input, options) {
-  options = Object.assign({}, { spellcheck: false, smartquots: true }, options || {});
+  options = Object.assign({}, { spellcheck: false, smartquotes: true }, options || {});
   var lex = Lexer();
   var lexResults = lex(input);
   var output = parse(input, lexResults.tokens, lexResults.positions, options);
