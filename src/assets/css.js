97f663501655e2c807481120ebda4d0704796db4
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-05-09 09:33:53 -0400

    Check for optional CSS path before inspecting it

--- src/assets/css.js
+++ src/assets/css.js
@@ -6,7 +6,7 @@ const THEMES_DIR = join(__dirname, '..', 'themes');
 
 module.exports = function (options) {
   const { layout, theme, css } = options;
-  const pathPrefix = isAbsolute(css) ? '' : process.cwd();
+  const pathPrefix = css && isAbsolute(css) ? '' : process.cwd();
   const layoutCSS = readFileSync(join(LAYOUTS_DIR, layout + '.css'));
   const themeCSS = readFileSync(join(THEMES_DIR, theme + '.css'));
   const customCSS = css ? readFileSync(join(pathPrefix, css)) : '';
