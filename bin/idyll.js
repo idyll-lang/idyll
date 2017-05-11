aa8678fdfda4958189124d362be35b67255ba0fe
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-05-10 22:20:08 -0400

    Update CLI API, ditch minimist in favor of yargs, bunch o’ path clean up

--- bin/idyll.js
+++ bin/idyll.js
@@ -1,53 +1,65 @@
 #! /usr/bin/env node
+
 const idyll = require('../src/');
-const path = require('path');
-const isPath = (str) => {
-  return (str.indexOf('/') > -1 || str.indexOf('\\') > -1);
+
+var argv = require('yargs')
+  .usage('Usage: idyll index.idl')
+  .example('$0 index.idl --build', 'Turn index.idl into output')
+  .demandCommand(1)
+  .alias({
+    b: 'build',
+    m: 'components',
+    c: 'css',
+    d: 'datasets',
+    q: 'defaultComponents',
+    f: 'inputFile',
+    s: 'inputString',
+    l: 'layout',
+    o: 'output',
+    k: 'spellcheck',
+    t: 'template',
+    e: 'theme'
+  })
+  .boolean('build')
+  .describe('build', 'Generate output files and exit')
+  .default('build', false)
+  .describe('components', 'Directory where components are located')
+  .default('components', 'components')
+  .describe('css', 'Custom CSS file to include in output')
+  .describe('datasets', 'Directory where data files are located')
+  .default('datasets', 'data')
+  .describe('defaultComponents', 'Directory where default set of components are located')
+  .default('defaultComponents', 'components/default')
+  .describe('inputFile', 'File containing Idyll source')
+  .describe('inputString', 'Idyll source as a string')
+  .describe('layout', 'Name of (or path to) the layout to use')
+  .choices('layout', ['blog', 'centered', 'none'])
+  .default('layout', 'blog')
+  .describe('output', 'Directory where built files should be written')
+  .default('output', 'build')
+  .boolean('spellcheck')
+  .default('spellcheck', true)
+  .describe('spellcheck', 'Check spelling of Idyll source input')
+  .describe('template', 'Path to HTML template')
+  .default('template', '_index.html')
+  .describe('theme', 'Name of (or path to) the theme to use')
+  .choices('theme', ['idyll', 'github', 'none'])
+  .default('theme', 'idyll')
+  .alias('h', 'help')
+  .argv;
+
+// inputFile can be passed as non-hypenated argument
+if (argv._[0]) argv.f = argv.inputFile = argv._[0];
+
+// move spellcheck down a level
+argv.compilerOptions = {
+  spellcheck: argv.spellcheck
 };
 
-const argv = require('minimist')(process.argv.slice(2), {
-  boolean: ['spellcheck'],
-  default: {
-    spellcheck: true
-  }
-});
+// delete stuff we don't need
+delete argv._;
+delete argv['$0'];
+delete argv.k;
+delete argv.spellcheck;
 
-let options = {
-  compilerOptions: {
-    spellcheck: argv.spellcheck
-  },
-  build: argv.build ? argv.build : false
-};
-
-if (argv.hasOwnProperty('output')) {
-  options.output = argv.output;
-}
-if (argv.hasOwnProperty('template')) {
-  options.htmlTemplate = argv.template;
-}
-if (argv.hasOwnProperty('css')) {
-  options.css = argv.css;
-}
-if (argv.hasOwnProperty('components')) {
-  options.componentFolder = argv.components;
-}
-if (argv.hasOwnProperty('default-components')) {
-  options.defaultComponents = argv['default-components'];
-}
-if (argv.hasOwnProperty('datasets')) {
-  options.dataFolder = argv.datasets;
-}
-if (argv.hasOwnProperty('layout')) {
-  options.layout = argv.layout;
-  if (isPath(options.layout)) {
-    options.layout = path.resolve(options.layout);
-  }
-}
-if (argv.hasOwnProperty('theme')) {
-  options.theme = argv.theme;
-  if (isPath(options.theme)) {
-    options.theme = path.resolve(options.theme);
-  }
-}
-
-idyll(path.resolve(argv._[0]), options);
+idyll(argv);
