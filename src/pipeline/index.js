7998530abee94d8cf3a4bff1accb6a58f1634d43
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-05-19 10:02:28 -0400

    Move file writing into pipeline and ensure compile errors are handled

--- src/pipeline/index.js
+++ src/pipeline/index.js
@@ -10,11 +10,9 @@ const {
   getHTML
 } = require('./parse');
 const css = require('./css');
-const write = require('./write-artifacts');
 const bundleJS = require('./bundle-js');
-const writeJS = require('./write-js');
 
-let outputs = {};
+let tree;
 
 const build = (opts, paths, inputConfig) => {
   // always store source in opts.inputString
@@ -22,34 +20,51 @@ const build = (opts, paths, inputConfig) => {
     opts.inputString = fs.readFileSync(paths.IDYLL_INPUT_FILE, 'utf8');
   }
 
-  const ast = compile(opts.inputString, opts.compilerOptions);
-  const tree = {
-    ast: getASTJSON(ast),
-    components: getComponentsJS(ast, paths, inputConfig),
-    css: css(opts),
-    data: getDataJS(ast, paths.DATA_DIR),
-    html: getHTML(ast, fs.readFileSync(paths.HTML_TEMPLATE_FILE, 'utf8'))
-  };
-
-  return write(tree, paths)
-    .then(() => {
-      return bundleJS(paths);
-    })
-    .then((js) => {
-      if (opts.minify) {
-        js = UglifyJS.minify(js, {fromString: true}).code;
-      }
-      tree.js = js;
-      return writeFile(paths.JS_OUTPUT_FILE, tree.js);
-    })
-    .then(() => {
-      return tree;
-    });
+  return Promise.try(
+    // this is all synchronous so we wrap it with Promise.try
+    // to start a Promise chain and turn any synchronous exceptions into a rejection
+    () => {
+      const ast = compile(opts.inputString, opts.compilerOptions);
+      tree = {
+        ast: getASTJSON(ast),
+        components: getComponentsJS(ast, paths, inputConfig),
+        css: css(opts),
+        data: getDataJS(ast, paths.DATA_DIR),
+        html: getHTML(ast, fs.readFileSync(paths.HTML_TEMPLATE_FILE, 'utf8'))
+      };
+    }
+  )
+  .then(() => {
+    // write everything but JS to disk
+    return Promise.all([
+      writeFile(paths.AST_OUTPUT_FILE, tree.ast),
+      writeFile(paths.COMPONENTS_OUTPUT_FILE, tree.components),
+      writeFile(paths.CSS_OUTPUT_FILE, tree.css),
+      writeFile(paths.DATA_OUTPUT_FILE, tree.data),
+      writeFile(paths.HTML_OUTPUT_FILE, tree.html),
+    ]);
+  })
+  .then(() => {
+    return bundleJS(paths); // create index.js bundle
+  })
+  .then((js) => {
+    // minify bundle if necessary and store it
+    if (opts.minify) {
+      js = UglifyJS.minify(js, {fromString: true}).code;
+    }
+    tree.js = js;
+  })
+  .then(() => {
+    writeFile(paths.JS_OUTPUT_FILE, tree.js); // write JS to disk
+  })
+  .then(() => {
+    return tree; // return all results
+  });
 }
 
 const updateCSS = (opts, paths) => {
-  outputs.css = css(opts);
-  return writeFile(paths.CSS_OUTPUT_FILE, outputs.css);
+  tree.css = css(opts);
+  return writeFile(paths.CSS_OUTPUT_FILE, tree.css);
 }
 
 module.exports = {
