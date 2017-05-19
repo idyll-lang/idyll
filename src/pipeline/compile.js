4e3be45e9aa3cf16850b3e24482fbb038cab99bc
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-05-19 10:03:10 -0400

    Remove unused file

--- src/pipeline/compile.js
+++
@@ -1,6 +0,0 @@
-const compile = require('idyll-compiler');
-const Promise = require('bluebird');
-
-module.exports = function (idyllSource, compilerOptions = {}) {
-  return compile(idyllSource, compilerOptions);
-};
