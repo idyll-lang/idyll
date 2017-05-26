28d772a2f841350e3bf19490c443b05631abc2ee
Ricky Reusser <rsreusser@gmail.com>
2017-05-25 22:22:43 -0400

    Revert directory order

--- src/index.js
+++ src/index.js
@@ -77,7 +77,7 @@ const idyll = (options = {}, cb) => {
               logLevel: 'warn',
               logPrefix: 'Idyll',
               notify: false,
-              server: [paths.INPUT_DIR, paths.OUTPUT_DIR],
+              server: [paths.OUTPUT_DIR, paths.INPUT_DIR],
               ui: false
             });
           }
