bf9fee89da5b13613638138c840880b285098da0
Matthew Conlen <mc@mathisonian.com>
2017-05-26 12:39:23 -0700

    cleanup components

--- components/vega-lite.js
+++ components/vega-lite.js
@@ -1,7 +1,5 @@
 const React = require('react');
 const IdyllComponent = require('idyll-component');
-const d3 = require('./lib/d3');
-window.d3 = d3;
 const VL = require('react-vega-lite').default;
 
 class VegaLite extends IdyllComponent {
