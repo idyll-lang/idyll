b2c9929ec70126d62e111c1e730bea7c3589e3b2
Matthew Conlen <mc@mathisonian.com>
2017-05-27 01:39:53 -0700

    revert ignore

--- src/pipeline/bundle-js.js
+++ src/pipeline/bundle-js.js
@@ -14,7 +14,7 @@ const getTransform = (opts, paths) => {
     return (opts[name] || []).map(d => require(d));
   };
 
-  return [[ babelify, { presets: [ reactPreset, es2015Preset ], ignore: ['node_modules/!(idyll)/**/*'] } ]]
+  return [[ babelify, { presets: [ reactPreset, es2015Preset ] } ]]
     .concat(_getTransform('transform'))
     .concat([[ brfs ]]);
 };
