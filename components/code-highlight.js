a6e96f71ccf06c866e0c10cbf42c2d10a6c52002
Matthew Conlen <mc@mathisonian.com>
2017-06-06 16:59:14 -0700

    revert

--- components/code-highlight.js
+++ components/code-highlight.js
@@ -3,22 +3,9 @@ const IdyllComponent = require('idyll-component');
 import SyntaxHighlighter from "react-syntax-highlighter/dist/light";
 import style from 'react-syntax-highlighter/dist/styles/github';
 
-const languageMap = {
-  js: 'javascript',
-  html: 'htmlbars'
-};
-
-const getLanguage = (l) => {
-  if (languageMap[l]) {
-    return languageMap[l];
-  }
-  return l;
-}
-
 class CodeHighlight extends IdyllComponent {
-
   render() {
-    return <SyntaxHighlighter style={style} language={getLanguage(this.props.language)}>{this.props.children[0]}</SyntaxHighlighter>;
+    return <SyntaxHighlighter style={style} language={this.props.language}>{this.props.children[0]}</SyntaxHighlighter>;
   }
 }
 
