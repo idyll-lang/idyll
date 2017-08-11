40ecf990537db62554a076ac59fcd9271538f9c6
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-08-11 11:26:09 -0400

    More vars and derived vars tests, including updates

--- packages/idyll-document/test/vars/vars.js
+++ packages/idyll-document/test/vars/vars.js
@@ -5,12 +5,28 @@ import ast from './ast.json'
 
 describe('Component state initialization', () => {
   it('creates the expected state', () => {
-    const doc = new InteractiveDocument({ast})
+    const doc = new InteractiveDocument({ast});
     expect(doc.state).toEqual({x: 2, frequency: 1});
   });
 
   it('creates the expected derived vars', () => {
-    const doc = new InteractiveDocument({ast})
+    const doc = new InteractiveDocument({ast});
+    expect(doc.derivedVars).toEqual({
+      xSquared: {
+        value: 4,
+        update: expect.any(Function)
+      }
+    });
+  });
+
+  it('can return the expected derived var values', () => {
+    const doc = new InteractiveDocument({ast});
     expect(doc.getDerivedVars()).toEqual({xSquared: 4});
+  });
+
+  it('can update the derived vars', () => {
+    const doc = new InteractiveDocument({ast});
+    doc.derivedVars.xSquared.update({x: 3, frequency: 5});
+    expect(doc.getDerivedVars()).toEqual({xSquared: 9});
   });
 });
