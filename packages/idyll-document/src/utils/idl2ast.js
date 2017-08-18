213ae2889cc0d0acdcdaf324027127d8ab9f6316
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-08-18 10:38:00 -0400

    Add AST to schema step in util

--- packages/idyll-document/src/utils/idl2ast.js
+++ packages/idyll-document/src/utils/idl2ast.js
@@ -1,11 +1,26 @@
 const compiler = require('idyll-compiler')
 const fs = require('fs')
 const { join } = require('path')
+const { splitAST, translate } = require('./index')
 
 fs.writeFileSync(
   join(__dirname, 'ast.json'),
   JSON.stringify(
     compiler(fs.readFileSync(join(__dirname, 'src.idl'), 'utf8')),
+    null,
+    2
+  ),
+  'utf8'
+)
+
+fs.writeFileSync(
+  join(__dirname, 'schema.json'),
+  JSON.stringify(
+    translate(
+      splitAST(
+        JSON.parse(fs.readFileSync(join(__dirname, 'ast.json'), 'utf8'))
+      ).elements
+    ),
     null,
     2
   ),
