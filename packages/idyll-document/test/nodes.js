ed1309c2b1b24bd75c10522c1f961e30c3eb8660
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-08-19 10:52:05 -0400

    Basic schema duplication and modification tests

--- packages/idyll-document/test/nodes.js
+++ packages/idyll-document/test/nodes.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import componentClasses from 'idyll-components';
 
 import IdyllDocument from '../src/';
-import { translate } from '../src/utils'
+import { translate, walkSchema } from '../src/utils'
 import ReactJsonSchema from '../src/utils/schema2element';
 import ast from './fixtures/ast.json'
 import schema from './fixtures/schema.json'
@@ -11,6 +11,76 @@ import schema from './fixtures/schema.json'
 describe('AST to Schema', () => {
   it('converts from AST to schema expected by ReactJsonSchema', () => {
     expect(translate(ast)).toEqual(schema)
+  })
+})
+
+describe('Schema transformations', () => {
+  const source = [{
+    "component": "p",
+    "children": [
+      "This document is being rendered from ",
+      {
+        "component": "strong",
+        "children": [
+          "Idyll markup"
+        ]
+      },
+      ". If you’ve used ",
+      {
+        "component": "a",
+        "href": "https://daringfireball.net/projects/markdown/",
+        "children": [
+          "markdown"
+        ]
+      },
+      ", Idyll should look pretty familiar, but it has some additional features. Write text in the box on the left and the output on the right will automatically update."
+    ]
+  }];
+
+  it('can copy schema', () => {
+    const d = walkSchema(x => true, source, x => x)
+    expect(d).toEqual(source)
+  })
+
+  it('can modify schema', () => {
+    const modified = [{
+      "component": "p",
+      "children": [
+        "This document is being rendered from ",
+        {
+          "component": "wrapper",
+          "style": {color: 'red'},
+          "children": [
+            {
+              "component": "strong",
+              "children": ["Idyll markup"]
+            }
+          ]
+        },
+        ". If you’ve used ",
+        {
+          "component": "a",
+          "href": "https://daringfireball.net/projects/markdown/",
+          "children": [
+            "markdown"
+          ]
+        },
+        ", Idyll should look pretty familiar, but it has some additional features. Write text in the box on the left and the output on the right will automatically update."
+      ]
+    }];
+
+    const d = walkSchema(x => true, source, x => {
+      if (x.component === 'strong') {
+        return {
+          component: 'wrapper',
+          style: { color: 'red' },
+          children: [x]
+        }
+      }
+      return x
+    })
+
+    expect(d).toEqual(modified)
   })
 });
 
