34faf234d1150aa5b564344c62f8fe3cb30660a5
Matthew Conlen <mc@mathisonian.com>
2017-09-26 22:06:23 -0700

    table initial commit

--- packages/idyll-components/src/table.js
+++ packages/idyll-components/src/table.js
@@ -1,15 +1,47 @@
-import React from 'react';
-const Reactable = require('reactable');
-const Table = Reactable.Table;
-const Tr = Reactable.Tr;
-const Td = Reactable.Td;
+const React = require('react');
+const Table = require('react-table').default;
 
-class TableComponent extends React.PureComponent {
+class TableComponent extends React.Component {
+  getColumns() {
+    if (this.props.columns) {
+      if (this.props.columns.length && typeof this.props.columns[0] === 'string') {
+        return this.props.columns.map((d) => {
+          return {
+            Header: d,
+            accessor: d
+          };
+        })
+      }
+
+      return this.props.columns;
+    }
+    if ((this.props.data || []).length) {
+      return Object.keys(this.props.data[0]).map((d) => {
+        return {
+          Header: d,
+          accessor: d
+        }
+      })
+    }
+
+    return [];
+  }
   render() {
     return (
-      <Table className={`table ${this.props.className || ''}`} data={this.props.data} />
+      <Table
+        className={`table ${this.props.className || ''}`}
+        minRows={(this.props.data || []).length}
+        {...this.props}
+        columns={this.getColumns()}
+         />
     );
   }
 }
 
-export default TableComponent;
+TableComponent.defaultProps = {
+  showPagination: false,
+  showPageSizeOptions: false,
+  showPageJump: false
+}
+
+module.exports = TableComponent;
