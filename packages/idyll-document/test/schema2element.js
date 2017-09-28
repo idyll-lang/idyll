e9e2d9c859f52faeef325ceac375f72542184909
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2017-09-28 16:29:11 -0400

    JSON schema to React element tests

---
+++ packages/idyll-document/test/schema2element.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Enzyme, { mount, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import * as components from 'idyll-components';
+import ReactJsonSchema from '../src/utils/schema2element';
+import schema from './fixtures/schema.json';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('ReactJsonSchema', () => {
+
+  it('can be created without arguments', () => {
+    const rjs = new ReactJsonSchema();
+    expect(rjs).toBeDefined();
+  });
+
+  it('can accept a componentMap in the constructor', () => {
+    const rjs = new ReactJsonSchema(components);
+    expect(rjs.getComponentMap()).toBe(components);
+  });
+
+  it('can set the componentMap after the constructor', () => {
+    const rjs = new ReactJsonSchema();
+    rjs.setComponentMap(components);
+    expect(rjs.getComponentMap()).toBe(components);
+  });
+
+  it('can parse a schema', () => {
+    const rjs = new ReactJsonSchema(components);
+    const tree = rjs.parseSchema({component: 'div', children: schema});
+    expect(shallow(tree)).toMatchSnapshot();
+  });
+
+});
