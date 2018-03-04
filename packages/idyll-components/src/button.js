3013bc1cba9432f325ab8268112a0e9b2a98427c
Matthew Conlen <mc@mathisonian.com>
2018-03-03 22:46:32 -0800

    update button

--- packages/idyll-components/src/button.js
+++ packages/idyll-components/src/button.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Button extends React.PureComponent {
   render() {
     return (
-      <button onClick={this.props.onClick.bind(this)}>
+      <button {...props} onClick={this.props.onClick.bind(this)}>
         {this.props.children}
       </button>
     );
