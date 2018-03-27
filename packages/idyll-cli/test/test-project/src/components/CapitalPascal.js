688d17d505afa6a7c5c76302278a21e6d3155dd8
Matthew Conlen <mc@mathisonian.com>
2018-03-27 16:33:28 -0700

    cleanup test output

--- packages/idyll-cli/test/test-project/src/components/CapitalPascal.js
+++ packages/idyll-cli/test/test-project/src/components/CapitalPascal.js
@@ -2,7 +2,7 @@ const React = require('react');
 
 class PascalComponent extends React.PureComponent {
   render() {
-    const {hasError, updateProps, ...props} = this.props;
+    const {hasError, updateProps, idyll, ...props} = this.props;
 
     return (
       <div {...props}>
