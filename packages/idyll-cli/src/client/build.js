150af2b60434f9e7f624fcbd1821b5fd6c2cc67d
Matthew Conlen <mc@mathisonian.com>
2018-05-06 18:44:43 -0700

    remove debug statements

--- packages/idyll-cli/src/client/build.js
+++ packages/idyll-cli/src/client/build.js
@@ -10,11 +10,8 @@ const datasets = require('__IDYLL_DATA__');
 require('__IDYLL_SYNTAX_HIGHLIGHT__');
 
 const opts = require('__IDYLL_OPTS__');
-console.log(opts);
 const { context, layout, theme } = opts;
 
-console.log('INITIALIZING WITH CONTEXT');
-console.log(context);
 const mountMethod = opts.ssr ? 'hydrate' : 'render';
 ReactDOM[mountMethod](
   React.createElement(IdyllDocument, { ast, components, context, datasets, layout, theme }),
