e0e99944d2b550a80d4a858dd8ff8cfe5647bbc6
Matthew Conlen <mc@mathisonian.com>
2018-06-25 01:40:34 -0700

    update styles for project template

--- packages/idyll-template-projects/templates/basic/components/custom-component.js
+++ packages/idyll-template-projects/templates/basic/components/custom-component.js
@@ -5,7 +5,14 @@ class CustomComponent extends React.Component {
     const { hasError, idyll, updateProps, ...props } = this.props;
     return (
       <div {...props}>
-        This is a custom component
+        <svg width={300} height={50} style={{ display: 'block', margin: '20px auto', background: 'white'}}>
+          <rect width={50} height={50} y={0} x={25} fill={'#ddd'} />
+          <circle cx={50} cy={25} r={15} x={25} fill={'#000'} />
+          <rect width={50} height={50} y={0} x={125} fill={'#ddd'} />
+          <circle cx={150} cy={25} r={15} x={25} fill={'#000'} />
+          <rect width={50} height={50} y={0} x={225} fill={'#ddd'} />
+          <circle cx={250} cy={25} r={15} x={25} fill={'#000'} />
+        </svg>
       </div>
     );
   }
