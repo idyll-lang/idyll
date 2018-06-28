b6ad221f5b4a5f71a4602da3d3d96ee2460cbd1d
Megan Vo <meganvo@cs.washington.edu>
2018-06-27 18:27:22 -0700

    change css style

--- packages/idyll-docs/pages/docs/publishing/embedding.js
+++ packages/idyll-docs/pages/docs/publishing/embedding.js
@@ -2,30 +2,31 @@ import Link from 'next/link'
 import markdown from 'markdown-in-js'
 import Layout from '../../../components/layout'
 
-export default ({ url }) => (
-  
-  <Layout url={ url }>
-    <link rel="stylesheet"
-    href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/darcula.min.css" />
-    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
-    <script>hljs.initHighlightingOnLoad();</script>
-    <div>
-      <h1>Embedding Idyll in an existing web page</h1>
 
-      <p>The Idyll runtime is available as a React
-      component, allowing you to embed interactive
+export default ({ url }) => (
+
+    <Layout url={url}>
+        <div>
+            <link rel="stylesheet"
+                href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/ocean.min.css" />
+            <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+            <script>hljs.initHighlightingOnLoad();</script>
+            <h1>Embedding Idyll in an existing web page</h1>
+
+            <p>The Idyll runtime is available as a React
+            component, allowing you to embed interactive
       Idyll content anywhere on the web.</p>
 
-      <p>To do this, you must first install the dependencies:</p>
+            <p>To do this, you must first install the dependencies:</p>
 
-      <pre><code class="sh language-sh">$ npm i --save idyll-document idyll-components
+            <pre><code class="sh language-sh">$ npm i --save idyll-document idyll-components
       </code></pre>
 
-      <p>then, add it to your page. If you are already using React, you
+            <p>then, add it to your page. If you are already using React, you
       can include this as a standard component:</p>
 
-      <pre><code class="javascript language-javascript">
-{`import IdyllDocument from 'idyll-document';
+            <pre><code class="javascript language-javascript">
+                {`import IdyllDocument from 'idyll-document';
 import * as components from 'idyll-components';
 
 // An example functional component
@@ -38,17 +39,17 @@ import * as components from 'idyll-components';
   )
 }
 `}
+            </code></pre>
+
+            <p>If not, you'll also need to install <code>react</code> and <code>react-dom</code>:</p>
+
+            <pre><code class="sh language-sh">$ npm i --save react react-dom
       </code></pre>
 
-      <p>If not, you'll also need to install <code>react</code> and <code>react-dom</code>:</p>
+            <p>and can embed it like this:</p>
 
-      <pre><code class="sh language-sh">$ npm i --save react react-dom
-      </code></pre>
-
-      <p>and can embed it like this:</p>
-
-      <pre><code class="javascript language-javascript">
-{`import React from 'react';
+            <pre><code class="javascript language-javascript">
+                {`import React from 'react';
 import ReactDOM from 'react-dom';
 import IdyllDocument from 'idyll-document';
 import * as components from 'idyll-components';
@@ -63,8 +64,8 @@ ReactDOM.render(
   containerElement
 )
 `}
-      </code></pre>
-      <pre><code>var deleteMe = 5;</code></pre>
-    </div>
-  </Layout>
+            </code></pre>
+            <pre><code>var deleteMe = 5;</code></pre>
+        </div>
+    </Layout>
 )
