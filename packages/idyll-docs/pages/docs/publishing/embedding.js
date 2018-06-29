3badb540de13017dcc02119a1442d8ef965a9115
Megan Vo <meganvo@cs.washington.edu>
2018-06-29 14:06:39 -0700

    add initial react-highlight code to embedding.js

--- packages/idyll-docs/pages/docs/publishing/embedding.js
+++ packages/idyll-docs/pages/docs/publishing/embedding.js
@@ -1,39 +1,40 @@
 import Link from 'next/link'
 import markdown from 'markdown-in-js'
 import Layout from '../../../components/layout'
+import React from 'react'
+import Highlight from 'react-highlight'
+
 // TODO ask if we should keep the local css/highlight.js file if we're just gonna end up using server instead
 // cause if we can get the local stuff to work, we can easily change the background color.
 // Also, flipping between pages does not make highlight load (possibly bc of highlightOnLoad command)
 // so must have a work around
 
 var codeStyle = {
-    background: '#4C4B63'
+  background: '#4C4B63'
 };
 
 export default ({ url }) => (
+  <Layout url={url}>
+    <link rel="stylesheet" href="../../../../idyll/node_modules/highlight.js/styles/tomorrow-night-eighties.css" />
+    <div>
 
-    <Layout url={url}>
-        <div>
-            <link rel="stylesheet"
-                href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/tomorrow-night-eighties.min.css" />
-            <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
-            <script>hljs.initHighlightingOnLoad();</script>
-            <h1>Embedding Idyll in an existing web page</h1>
+      <h1>Embedding Idyll in an existing web page</h1>
 
-            <p>The Idyll runtime is available as a React
-            component, allowing you to embed interactive
+      <p>The Idyll runtime is available as a React
+      component, allowing you to embed interactive
       Idyll content anywhere on the web.</p>
 
-            <p>To do this, you must first install the dependencies:</p>
+      <p>To do this, you must first install the dependencies:</p>
 
-            <pre><code class="sh language-sh">$ npm i --save idyll-document idyll-components
+      <pre><code class="sh language-sh" style={codeStyle}>$ npm i --save idyll-document idyll-components
       </code></pre>
 
-            <p>then, add it to your page. If you are already using React, you
+      <p>then, add it to your page. If you are already using React, you
       can include this as a standard component:</p>
 
-            <pre><code class="javascript language-javascript" style={codeStyle}>
-                {`import IdyllDocument from 'idyll-document';
+      <pre><code class="javascript language-javascript">
+        <Highlight className='React'>
+          {`import IdyllDocument from 'idyll-document';
 import * as components from 'idyll-components';
 
 // An example functional component
@@ -46,17 +47,18 @@ import * as components from 'idyll-components';
   )
 }
 `}
-            </code></pre>
-
-            <p>If not, you'll also need to install <code>react</code> and <code>react-dom</code>:</p>
-
-            <pre><code class="sh language-sh">$ npm i --save react react-dom
+        </Highlight>
       </code></pre>
 
-            <p>and can embed it like this:</p>
+      <p>If not, you'll also need to install <code>react</code> and <code>react-dom</code>:</p>
 
-            <pre><code class="javascript language-javascript">
-                {`import React from 'react';
+      <pre><code class="sh language-sh">$ npm i --save react react-dom
+      </code></pre>
+
+      <p>and can embed it like this:</p>
+
+      <pre><code class="javascript language-javascript">
+        {`import React from 'react';
 import ReactDOM from 'react-dom';
 import IdyllDocument from 'idyll-document';
 import * as components from 'idyll-components';
@@ -71,9 +73,9 @@ ReactDOM.render(
   containerElement
 )
 `}
-            </code></pre>
-            <pre><code>var deleteMe = 5;</code></pre>
-        </div>
-    </Layout>
+      </code></pre>
+      <pre><code>var deleteMe = 5;</code></pre>
+    </div>
+  </Layout>
 )
 
