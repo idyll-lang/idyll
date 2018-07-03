372b20f53b0fe09238d79352a6443c40c8a165b9
Megan Vo <meganvo@cs.washington.edu>
2018-07-02 23:49:00 -0700

    change formatting to custom.js and add syntax highlighting

--- packages/idyll-docs/pages/docs/components/custom.js
+++ packages/idyll-docs/pages/docs/components/custom.js
@@ -1,27 +1,24 @@
 import Link from 'next/link'
 import Layout from '../../../components/layout'
-import showdown from 'showdown'
+import Highlight from 'react-highlight'
+import markdown from 'markdown-in-js'
 
-showdown.setFlavor('github')
+export default ({ url }) => (
+  <Layout url={url}>
+    <link rel="stylesheet" href="../../../static/styles/tomorrow-night-eighties.css" />
+    <div>
+      <h1>Custom Components</h1>
+      <h2>Overview</h2>
 
-
-const mdConverter = new showdown.Converter()
-
-
-const Content = () => mdConverter.makeHtml(`
-# Custom Components
-
-## Overview
-
-Under the hood an Idyll component is anything that will
+      <p>Under the hood an Idyll component is anything that will
 function as a React component. If you create a custom component in
 JavaScript, point Idyll to the folder where you created it and it will
-be available in your markup.
+be available in your markup.</p>
 
-For example, this custom component
+      <p>For example, this custom component</p>
 
-\`\`\`js
-// custom.js
+      <Highlight className='javascript'>
+        {`// custom.js
 const React = require('react');
 
 class Custom extends React.Component {
@@ -36,32 +33,29 @@ class Custom extends React.Component {
   }
 }
 
-module.exports = Custom;
-\`\`\`
+module.exports = Custom;`}
+      </Highlight>
 
-would be invoked inside of an Idyll file with the
-following code:
+      <p>would be invoked inside of an Idyll file with the
+following code:</p>
 
-\`\`\`
-[Custom /]
-\`\`\`
+      <pre><code>{`[Custom /]`}</code></pre>
 
-## Updating the Document
+      <h2>Updating the Document</h2>
 
-Idyll provides a way for your components to push state changes back up to the
+      <p>Idyll provides a way for your components to push state changes back up to the
 document. This is how a component is able to affect change through the document.
-An \`updateProps\` method is injected onto the \`props\` object that your
-custom component receives. This function can be called to update variable values
-and propagate those changes to the rest of the Idyll document.
+An <code>updateProps</code> method is injected onto the <code>props</code> object that your
+                                                                                                                                                                                                        custom component receives. This function can be called to update variable values
+and propagate those changes to the rest of the Idyll document.</p>
 
-### Example
-
-For example, a component can change the value of a
+      <h3>Example</h3>
+      <p>For example, a component can change the value of a
 property that it receives, and Idyll will propagate
-the change to any bound variables on the page.
+the change to any bound variables on the page.</p>
 
-\`\`\`js
-const React = require('react');
+      <Highlight className='javascript'>
+        {`const React = require('react');
 class Incrementer extends React.Component {
 
   increment() {
@@ -79,49 +73,43 @@ class Incrementer extends React.Component {
   }
 }
 
-module.exports = Incrementer;
-\`\`\`
+module.exports = Incrementer;`}
+      </Highlight>
 
-The \`Incrementer\` component could then be used as follows:
+      <p>The <code>Incrementer</code> component could then be used as follows:</p>
 
-\`\`\`
-[var name:"clickCount" value:0 /]
+      <pre><code>{`[var name:"clickCount" value:0 /]
 
 [Incrementer value:clickCount /]
-[Display value:clickCount /]
-\`\`\`
+[Display value:clickCount /]`}</code></pre>
 
-Notice that even though the \`Incrementer\` component doesn't know
-anything about the variable \`clickCount\`, it will still correctly
-update.
+      <p>Notice that even though the <code>Incrementer</code> component doesn't know
+      anything about the variable <code>clickCount</code>, it will still correctly
+update.</p>
 
+      <h2>Name resolution</h2>
 
-## Name resolution
+      <p>Components lookup is based on filenames. If your component name
+is <code>MyComponent</code>, it will match files like <code>mycomponent.js</code> or <code>my-component.js</code>.
+The component filenames are case insensitive.</p>
 
-Components lookup is based on filenames. If your component name
-is \`MyComponent\`, it will match files like \`mycomponent.js\` or \`my-component.js\`.
-The component filenames are case insensitive.
+      <p>Custom component are meant for times when more complex and custom
+      code is needed. By default Idyll will look for your custom components
+inside of a folder called <code>components/</code>. If you wish to change the custom
+        component path, specify it with the <code>--components</code> option, e.g.</p>
+      <p><code>idyll index.idl --css styles.css --components custom/component/path/</code>.</p>
 
-Custom component are meant for times when more complex and custom
-code is needed. By default Idyll will look for your custom components
-inside of a folder called \`components/\`. If you wish to change the custom
-component path, specify it with the \`--components\` option, e.g.
-\`idyll index.idl --css styles.css --components custom/component/path/\`.
+      <h2>Manipulating component children</h2>
 
-## Manipulating component children
+      <p>If your component needs to modify the render tree of its children,
+<a href="https://github.com/idyll-lang/idyll-component-children"> https://github.com/idyll-lang/idyll-component-children</a> is a helper library
+        to filter and map the component's children.</p>
 
-If your component needs to modify the render tree of its children,
-<https://github.com/idyll-lang/idyll-component-children> is a helper library
-to filter and map the component's children.
-`)
-
-export default ({ url }) => (
-  <Layout url={ url }>
-    <div dangerouslySetInnerHTML={ {__html: Content()} } />
-    <p>
-      Continue to learn how to use{' '}
-      <Link href="/docs/components/scrolling-and-refs"><a>references</a></Link>
-      {' '}to make your page more dynamic.
+      <p>
+        Continue to learn how to use{' '}
+        <Link href="/docs/components/scrolling-and-refs"><a>references</a></Link>
+        {' '}to make your page more dynamic.
     </p>
+    </div>
   </Layout>
 )
