466d884a452b3ecc9bc2a3e747af4e1a9a84ebdb
Alan Tan <taalantan@gmail.com>
2018-07-22 11:03:54 -0700

    remove webkit-position for now

--- packages/idyll-components/src/scroller.js
+++ packages/idyll-components/src/scroller.js
@@ -122,7 +122,7 @@ class Scroller extends React.Component {
       <div ref={(ref) => this.ref = ref} className="idyll-scroll" id={`idyll-scroll-${this.id}`} 
         style={Object.assign({ position: 'relative' }, { marginLeft: leftMarginAdjust})}>
         <div className="idyll-scroll-graphic"
-          style={Object.assign({ height: graphicHeight }, styles.SCROLL_GRAPHIC)} >
+          style={Object.assign({ height: graphicHeight, position: '-webkit-sticky' }, styles.SCROLL_GRAPHIC)} >
           <div style={Object.assign({ width: graphicWidth }, styles.SCROLL_GRAPHIC_INNER)}>
             {filterChildren(
               children,
