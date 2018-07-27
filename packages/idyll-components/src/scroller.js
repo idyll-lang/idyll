c9564a197b2cdfeb98b1ca03a54d44ea36ec4b48
Alan Tan <taalantan@gmail.com>
2018-07-27 14:56:20 -0700

    add debug for scroller

--- packages/idyll-components/src/scroller.js
+++ packages/idyll-components/src/scroller.js
@@ -21,7 +21,9 @@ const styles = {
     left: 0,
     right: 0,
     top: '50%',
-    transform: 'translateY(-50%)'
+    transform: 'translateY(-50%)',
+    objectFit: 'cover',
+    backgroundColor: 'blue' // for debugging
   }
 }
 
