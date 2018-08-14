28d85a6e4a24db31e878483dc094d325eb3cbbde
Alan Tan <taalantan@gmail.com>
2018-08-14 15:37:12 -0700

    remove unneeded comment

--- packages/idyll-document/src/components/overlay.js
+++ packages/idyll-document/src/components/overlay.js
@@ -4,8 +4,6 @@ class Overlay extends React.PureComponent {
 
   render() {
     const { idyll, updateProps, hasError, ...props } = this.props;
-    // I believe it should be looking at the component that this overlay is within, and then
-    // insert whatever icon on the top right of this component, let's say.
     return (
       <div className="overlay-div" {...props} />
     );
