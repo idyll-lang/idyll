c6411f6e369864b652203a9b25a4e23888828909
Matthew Conlen <mc@mathisonian.com>
2018-11-07 18:16:34 -0800

    add update notifier

--- packages/idyll-cli/bin/cli.js
+++ packages/idyll-cli/bin/cli.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
+
+updateNotifier({pkg}).notify();
+
 require('yargs')
   .usage('Usage: idyll <command> [options]')
   .commandDir('cmds')
