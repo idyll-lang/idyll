7f2081009dbfa444301aafba5f98bede9b1a626b
Matthew Conlen <mc@mathisonian.com>
2018-11-07 18:29:31 -0800

    move logic to other file

--- packages/idyll-cli/bin/cli.js
+++ packages/idyll-cli/bin/cli.js
@@ -1,10 +1,5 @@
 #!/usr/bin/env node
 
-const updateNotifier = require('update-notifier');
-const pkg = require('../package.json');
-
-updateNotifier({pkg}).notify();
-
 require('yargs')
   .usage('Usage: idyll <command> [options]')
   .commandDir('cmds')
