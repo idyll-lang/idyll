15fceb8f84e875064286518a2ecd74242cd5d99e
Matthew Conlen <mc@mathisonian.com>
2019-04-25 14:08:49 -0700

    inject CSS

--- packages/idyll-document/src/index.js
+++ packages/idyll-document/src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import Runtime from './runtime';
 import compile from 'idyll-compiler';
+import * as layouts from 'idyll-layouts';
+import * as themes from 'idyll-themes';
+
+const getLayout = layout => {
+  return layouts[layout.trim()] || {};
+};
+
+const getTheme = theme => {
+  return themes[theme.trim()] || {};
+};
 
 const defaultAST = {
   id: 0,
@@ -32,11 +42,27 @@ class IdyllDocument extends React.Component {
     };
   }
 
+  createStyleNode(str) {
+    var node = document.createElement('style');
+    node.innerHTML = str;
+    document.body.appendChild(node);
+    return node;
+  }
+
   componentDidMount() {
     if (!this.props.ast && this.props.markup) {
       compile(this.props.markup, this.props.compilerOptions).then(ast => {
         this.setState({ ast, hash: hashCode(this.props.markup), error: null });
       });
+    }
+
+    if (this.props.injectThemeCSS) {
+      this._themeNode = this.createStyleNode(getTheme(this.props.theme).styles);
+    }
+    if (this.props.injectLayoutCSS) {
+      this._layoutNode = this.createStyleNode(
+        getLayout(this.props.layout).styles
+      );
     }
   }
 
@@ -58,6 +84,23 @@ class IdyllDocument extends React.Component {
           this.setState({ previousAST: ast, ast, hash, error: null });
         })
         .catch(this.componentDidCatch.bind(this));
+    }
+
+    if (newProps.theme !== this.props.theme && newProps.injectThemeCSS) {
+      if (this._themeNode) {
+        this._themeNode.innerHTML = getTheme(newProps.theme);
+      } else {
+        this._themeNode = this.createStyleNode(getTheme(newProps.theme).styles);
+      }
+    }
+    if (newProps.layout !== this.props.layout && newProps.injectLayoutCSS) {
+      if (this._layoutNode) {
+        this._layoutNode.innerHTML = getLayout(newProps.layout);
+      } else {
+        this._layoutNode = this.createStyleNode(
+          getLayout(newProps.layout).styles
+        );
+      }
     }
   }
 
