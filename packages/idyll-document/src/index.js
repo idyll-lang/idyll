fddc56bd46fb49c39b961330633478f39bc20553
Matthew Conlen <mc@mathisonian.com>
2019-05-25 14:42:45 -0700

    improve hashing when AST is passed instead of markup

--- packages/idyll-document/src/index.js
+++ packages/idyll-document/src/index.js
@@ -37,7 +37,9 @@ class IdyllDocument extends React.Component {
     this.state = {
       ast: props.ast || defaultAST,
       previousAST: props.ast || defaultAST,
-      hash: '',
+      hash: props.ast
+        ? JSON.stringify(props.ast)
+        : props.hashCode(props.markup),
       error: null
     };
   }
@@ -90,6 +92,7 @@ class IdyllDocument extends React.Component {
     }
 
     if (newProps.ast) {
+      this.setState({ hash: JSON.stringify(newProps.ast) });
       return;
     }
 
