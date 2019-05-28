41b4f7857e75084487d6c4013875a519263448f7
Matthew Conlen <mc@mathisonian.com>
2019-05-28 16:05:27 -0700

    starting to update docs with better tutorials

--- packages/idyll-docs/pages/docs/getting-started.js
+++ packages/idyll-docs/pages/docs/getting-started.js
@@ -1,7 +1,6 @@
-import Link from 'next/link'
-import markdown from 'markdown-in-js'
-import Layout from '../../components/layout'
-
+import Link from 'next/link';
+import markdown from 'markdown-in-js';
+import Layout from '../../components/layout';
 
 const Content = () => markdown`
 # Getting Started
@@ -10,15 +9,17 @@ If you just want to quickly try out Idyll, we offer
 an [online editor](https://idyll-lang.github.io/editor) where you
 can play around and try out the language.
 
+<iframe width="560" height="315" src="https://www.youtube.com/embed/9YvEFdiY0yE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
 ## Installation
 
 To install and use Idyll locally, install it using \`npm\`.
 If you don't have \`npm\` installed, first install it by
 following [these instructions](https://www.npmjs.com/get-npm).
 
-\`\`\`sh
+~~~sh
 $ npm install -g idyll
-\`\`\`
+~~~
 
 Note that depending on how \`npm\` is installed, you may have to run the above command prefixed with \`sudo\`. Once the installation has finished, the \`idyll\` command will be available in your shell. To compile a file run \`$ idyll <input-file> --watch\`, this will compile and open the resulting webpage in your browser.
 
@@ -27,13 +28,13 @@ Note that depending on how \`npm\` is installed, you may have to run the above c
 Idyll provides a built-in project template to help you get started.
 To use the generator run
 
-\`\`\`sh
+~~~sh
 $ idyll create
-\`\`\`
+~~~
 
 and follow the prompts. The generator will produce a structure that looks like this:
 
-\`\`\`sh
+~~~sh
 $  tree -I node_modules
 .
 ├── components/
@@ -42,16 +43,16 @@ $  tree -I node_modules
 ├── index.idyll
 ├── package.json
 └── styles.css
-\`\`\`
+~~~
 
 The files do the following:
 
-* \`index.idyll\` - The main Idyll file, write your text in here.
-* \`styles.css\` - By putting CSS in here you can override the default styles.
-* \`components/\` - The folder for custom components. Any component defined in this folder can be invoked in the .idl file.
-* \`data/\` - If you want to include a dataset in your project, put it in here.
-* \`static/\` - A folder for static file such as images.
-* \`package.json\` - This file contains all the metadata for your project.
+- \`index.idyll\` - The main Idyll file, write your text in here.
+- \`styles.css\` - By putting CSS in here you can override the default styles.
+- \`components/\` - The folder for custom components. Any component defined in this folder can be invoked in the .idl file.
+- \`data/\` - If you want to include a dataset in your project, put it in here.
+- \`static/\` - A folder for static file such as images.
+- \`package.json\` - This file contains all the metadata for your project.
 
 After the project has been created, navigate to the root of the new folder, and run \`idyll\`.
 Idyll will compile your project and open it in your web browser. Every time you save the \`index.idyll\` file, the system will automatically recompile
@@ -59,14 +60,20 @@ everything and update the page in the browser.
 
 The \`components/default/\` folder contains the default components that Idyll provides. If you need to directly
 update one of these components or their styles, feel free - Idyll is built to be customized.
-`
-
+`;
 
 export default ({ url }) => (
-  <Layout url={ url } title={'Idyll Documentation | Installation and Getting Started'}>
+  <Layout
+    url={url}
+    title={'Idyll Documentation | Installation and Getting Started'}
+  >
     <Content />
     <p>
-      Continue to the <Link href="/docs/syntax"><a>next section</a></Link> to learn how to use Idyll's syntax.
+      Continue to the{' '}
+      <Link href="/docs/syntax">
+        <a>next section</a>
+      </Link>{' '}
+      to learn how to use Idyll's syntax.
     </p>
   </Layout>
-)
+);
