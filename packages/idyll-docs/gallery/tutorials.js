3af725dc95e8fbed81107efb8b1f72ba46e12929
Matthew Conlen <mc@mathisonian.com>
2019-06-11 15:24:00 -0700

    update tutorials page

--- packages/idyll-docs/gallery/tutorials.js
+++ packages/idyll-docs/gallery/tutorials.js
@@ -1,6 +1,6 @@
 const tutorialsGroups = [
   {
-    title: 'Tutorials gallery',
+    title: 'Design Patterns',
     items: [
       {
         label: 'Triggering Updates',
@@ -22,30 +22,6 @@ const tutorialsGroups = [
         sourceUrl: 'https://github.com/mathisonian/idyll-animation-article'
       },
       {
-        label: 'Stacking Scroller Components',
-        subtitle: 'Introduction to leveraging multiple Scrollers in Idyll',
-        href: 'https://idyll.pub/post/stack-scroll-0c3da7fb8e45f3ecb720bcf8/',
-        image: 'stacked-scroller.gif',
-        sourceUrl: 'https://github.com/megan-vo/stacked-scrolling-tutorial'
-      },
-      {
-        label: 'Using Mapbox with Idyll',
-        subtitle: 'Add interactive maps to your Idyll posts.',
-        href: 'https://mathisonian.github.io/idyll-map-example/',
-        image: 'mapbox.png',
-        sourceUrl: 'https://github.com/mathisonian/idyll-map-example'
-      },
-      {
-        label: 'Announcing idyll.pub',
-        subtitle:
-          "Publish interactive blog posts and explorable explanations with Idyll's free hosting service.",
-        href:
-          'https://idyll.pub/post/announcing-idyll-pub-0a3eff0661df3446a915700d/',
-        image:
-          'https://idyll.pub/post/announcing-idyll-pub-0a3eff0661df3446a915700d/static/images/share.png',
-        sourceUrl: 'https://github.com/mathisonian/announcing-idyll-pub'
-      },
-      {
         label: 'Scaffolding Interactives',
         subtitle:
           'Rapidly create scroll- and step-based interactives with Idyll',
@@ -54,13 +30,24 @@ const tutorialsGroups = [
         sourceUrl: 'https://github.com/mathisonian/scaffolding-interactives'
       },
       {
-        label: 'Take a Walk on the Idyll Side',
-        subtitle: 'Exploring new ways to create interactive documents',
-        href: 'https://mathisonian.github.io/idyll/a-walk-on-the-idyll-side/',
-        image: 'walk-on-the-idyll-side.png',
-        sourceUrl:
-          'https://github.com/mathisonian/idyll/tree/master/introducing-idyll'
+        label: 'Stacking Scroller Components',
+        subtitle: 'Introduction to leveraging multiple Scrollers in Idyll',
+        href: 'https://idyll.pub/post/stack-scroll-0c3da7fb8e45f3ecb720bcf8/',
+        image: 'stacked-scroller.gif',
+        sourceUrl: 'https://github.com/megan-vo/stacked-scrolling-tutorial'
       },
+      {
+        label: 'Dynamic Datasets',
+        subtitle:
+          'Using derived variables to easily create reactive views of data in Idyll.',
+        href: 'https://idyll.pub/post/derived-table-b00359641a4490037dc3dce7/',
+        image: 'dynamic-datasets.png'
+      }
+    ]
+  },
+  {
+    title: 'Library Integrations',
+    items: [
       {
         label: 'D3',
         href: 'https://idyll-lang.github.io/idyll-d3-component/',
@@ -69,17 +56,24 @@ const tutorialsGroups = [
           'https://github.com/idyll-lang/idyll-d3-component/tree/master/examples/basic'
       },
       {
+        label: 'Vega Lite',
+        href: 'https://idyll-lang.github.io/examples/csv/',
+        image: 'vl.png',
+        sourceUrl: 'https://github.com/idyll-lang/examples/tree/master/csv'
+      },
+      {
+        label: 'Mapbox',
+        subtitle: 'Add interactive maps to your Idyll posts.',
+        href: 'https://mathisonian.github.io/idyll-map-example/',
+        image: 'mapbox.png',
+        sourceUrl: 'https://github.com/mathisonian/idyll-map-example'
+      },
+      {
         label: 'regl',
         href: 'https://idyll-lang.github.io/idyll-regl-component/',
         image: 'regl.png',
         sourceUrl:
           'https://github.com/idyll-lang/idyll-regl-component/tree/master/examples/basic'
-      },
-      {
-        label: 'Vega Lite',
-        href: 'https://idyll-lang.github.io/examples/csv/',
-        image: 'vl.png',
-        sourceUrl: 'https://github.com/idyll-lang/examples/tree/master/csv'
       },
       {
         label: 'Firebase',
@@ -93,6 +87,29 @@ const tutorialsGroups = [
         image: 'apparatus.png',
         sourceUrl:
           'https://github.com/mathisonian/mathisonian.github.io/blob/master/pages/writing/apparatus.js'
+      }
+    ]
+  },
+  {
+    title: 'Idyll Overview',
+    items: [
+      {
+        label: 'Take a Walk on the Idyll Side',
+        subtitle: 'Exploring new ways to create interactive documents',
+        href: 'https://mathisonian.github.io/idyll/a-walk-on-the-idyll-side/',
+        image: 'walk-on-the-idyll-side.png',
+        sourceUrl:
+          'https://github.com/mathisonian/idyll/tree/master/introducing-idyll'
+      },
+      {
+        label: 'Publishing on idyll.pub',
+        subtitle:
+          'Easily publish Idyll articles online with our free hosting service.',
+        href:
+          'https://idyll.pub/post/announcing-idyll-pub-0a3eff0661df3446a915700d/',
+        image:
+          'https://idyll.pub/post/announcing-idyll-pub-0a3eff0661df3446a915700d/static/images/share.png',
+        sourceUrl: 'https://github.com/mathisonian/announcing-idyll-pub'
       }
     ]
   }
