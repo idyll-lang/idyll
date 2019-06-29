8f23ea146c67a3be343aa76b94598f51876ce906
amitbadala <amitbadala07@gmail.com>
2019-06-29 15:01:26 +0530

    check for token file

--- packages/idyll-cli/bin/cmds/clean.js
+++ packages/idyll-cli/bin/cmds/clean.js
@@ -1,10 +1,46 @@
 const fs = require('fs-extra');
-
+const inquirer = require('inquirer');
 exports.command = 'clean';
 exports.description = 'Remove .idyll folder';
 
 const PATH = './.idyll';
 
-fs.remove(PATH, err => {
-  if (err) return console.error(err);
-});
+exports.handler = async _ => {
+  try {
+    let isTokenExists = await checkIfTokenExists();
+    if (isTokenExists) {
+      let userAnswer = await confirmClean();
+      if (userAnswer === 'Yes') {
+        removeIdyll();
+      }
+    } else {
+      removeIdyll();
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+checkIfTokenExists = _ => {
+  return fs.pathExists(`${PATH}/token`);
+};
+
+confirmClean = async _ => {
+  const userResponse = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'confirmClean',
+      message: `This command will remove the idyll cache folder, including your idyll.pub token.
+      Subsequent deploys to idyll.pub will receive a new URL. 
+      Are you sure you wish to continue?`,
+      choices: ['Yes', 'No']
+    }
+  ]);
+  return userResponse.confirmClean;
+};
+
+removeIdyll = _ => {
+  fs.remove(PATH, err => {
+    if (err) return console.error(err);
+  });
+};
