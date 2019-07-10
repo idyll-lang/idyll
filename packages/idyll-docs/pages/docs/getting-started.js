cee6b9f45dc94ef970ec1095ea4d8ba176ff185e
Matthew Conlen <mc@mathisonian.com>
2019-07-10 13:35:24 -0700

    update docs copy

--- packages/idyll-docs/pages/docs/getting-started.js
+++ packages/idyll-docs/pages/docs/getting-started.js
@@ -5,23 +5,26 @@ import Layout from '../../components/layout';
 const Content = () => markdown`
 # Getting Started
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/9YvEFdiY0yE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+If you prefer video
+tutorials, [watch the introduction on YouTube](https://www.youtube.com/watch?v=9YvEFdiY0yE).
 
-If you just want to quickly try out Idyll, we offer
-an [online editor](https://idyll-lang.github.io/editor) where you
-can play around and try out the language.
+There's also an [online editor](https://idyll-lang.github.io/editor) where you
+can play around with the language.
 
 ## Installation
 
-To install and use Idyll locally, install it using \`npm\`.
-If you don't have \`npm\` installed, first install it by
-following [these instructions](https://www.npmjs.com/get-npm).
+_Idyll is a program that is used via the command line. If you aren't comfortable with the command line already, I'd recommend watching Daniel Shiffman's [short introduction](https://www.youtube.com/watch?v=oK8EvVeVltE) before starting._
+
+To use Idyll you must first install it using \`npm\`.
+If you don't have \`npm\`, first install it by following [these instructions](https://www.npmjs.com/get-npm).
 
 ~~~sh
 $ npm install -g idyll
 ~~~
 
-Note that depending on how \`npm\` is installed, you may have to run the above command prefixed with \`sudo\`. Once the installation has finished, the \`idyll\` command will be available in your shell. To compile a file run \`$ idyll <input-file> --watch\`, this will compile and open the resulting webpage in your browser.
+_Note that depending on how \`npm\` is installed, you may have to run the above command prefixed with \`sudo\`._
+
+Once the installation has finished, the \`idyll\` command will be available in your shell.
 
 ## Creating a project
 
@@ -32,36 +35,20 @@ $ idyll create
 ~~~
 
 to create a new project. You can choose either \`article\` or \`multipage\` from the
-to create a single post or a multipage setup (for making blogs, books, etc.).
+menu to create a single post or a multipage setup (for making blogs, books, etc.).
 
-For a single post, the generator will produce a structure that looks like this:
-
-~~~sh
-$  tree -I node_modules
-.
-├── components/
-├── data/
-├── static/
-├── index.idyll
-├── package.json
-└── styles.css
-~~~
-
-The files do the following:
+For a single post, the generator will produce the following files:
 
 - \`index.idyll\` - The main Idyll file, write your text in here.
-- \`styles.css\` - By putting CSS in here you can override the default styles.
-- \`components/\` - The folder for custom components. Any component defined in this folder can be invoked in the .idl file.
-- \`data/\` - If you want to include a dataset in your project, put it in here.
+- \`styles.css\` - Add any custom styles.
+- \`components/\` - Any component defined in this folder can be used in the idyll markup.
+- \`data/\` - If you want to include a dataset in your project, put it in here (supports CSV or JSON).
 - \`static/\` - A folder for static file such as images.
-- \`package.json\` - This file contains all the metadata for your project.
+- \`package.json\` - This file contains all the configuration for your project.
 
 After the project has been created, navigate to the root of the new folder, and run \`idyll\`.
 Idyll will compile your project and open it in your web browser. Every time you save the \`index.idyll\` file, the system will automatically recompile
 everything and update the page in the browser.
-
-The \`components/default/\` folder contains the default components that Idyll provides. If you need to directly
-update one of these components or their styles, feel free - Idyll is built to be customized.
 `;
 
 export default ({ url }) => (
@@ -75,7 +62,7 @@ export default ({ url }) => (
       <Link href="/docs/syntax">
         <a>next section</a>
       </Link>{' '}
-      to learn how to use Idyll's syntax.
+      to learn how to write Idyll markup.
     </p>
   </Layout>
 );
