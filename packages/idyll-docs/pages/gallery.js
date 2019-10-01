28a7736f05f0a7aab91440e9100a0d776c776935
jprask <jpraskys@gmail.com>
2019-10-01 10:56:03 -0300

    Improve mobile layout on gallery
    * Update gallery layout & basic-layout on mobile to give the content more space
    * Add a breakpoint in featured section of gallery where it becomes a column for small devices

--- packages/idyll-docs/pages/gallery.js
+++ packages/idyll-docs/pages/gallery.js
@@ -8,18 +8,8 @@ const Examples = () => (
   <section>
     <h1>Made with Idyll</h1>
 
-    <div
-      style={{
-        borderRadius: 5,
-        border: 'solid 1px #ddd',
-        background: '#fefefe',
-        padding: '1em',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-      }}
-    >
-      <div>
+    <div className="feature">
+      <div className="feature--text">
         <a style={{ fontSize: '1.25rem' }} href="https://parametric.press">
           Featured: Parametric Press, a digital magazine built with Idyll
         </a>
@@ -30,10 +20,10 @@ const Examples = () => (
         visual, and interactive capabilities of dynamic media are effectively
         combined.
       </div>
-      <div>
+      <div className="feature--image">
         <a href="https://parametric.press">
           <img
-            style={{ maxWidth: 300, height: 'auto' }}
+            style={{ maxWidth: 300, width: '100%', height: 'auto' }}
             src="https://parametric.press/issue-01/static/images/share.png"
           />
         </a>
@@ -73,6 +63,51 @@ const Examples = () => (
     {examples.map(eg => (
       <GalleryGroup {...eg} key={eg.title} />
     ))}
+
+    <style jsx>{`
+      .feature {
+        border-radius: 5px;
+        border: solid 1px #ddd;
+        background: #fefefe;
+        padding: 1em;
+        display: flex;
+        flex-direction: row;
+        align-items: flex-start;
+        justify-content: space-between;
+      }
+
+      .feature--text {
+        padding-right: 4em;
+        max-width: 64%;
+        flex: 2 2 64%;
+      }
+
+      .feature--image {
+        flex: 1 1 300px;
+        max-width: 300px;
+        max-height: 150px;
+      }
+
+      @media (max-width: 769px) {
+        .feature {
+          padding: 0.5em;
+          flex-direction: column;
+          align-items: center;
+        }
+
+        .feature--text {
+          padding: 0;
+          text-align: center;
+          max-width: 100%;
+          flex-basis: 100%;
+        }
+
+        .feature--image {
+          margin-top: 1em;
+          max-width: 100%;
+        }
+      }
+    `}</style>
   </section>
 );
 
