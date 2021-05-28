8e0172f0c68bc02a0b55cb243a6f9fb4a3a2ed31
Jasen Lo <jasenlo123@gmail.com>
2021-05-28 02:29:58 -0700

    graphic in the render tree!

--- packages/idyll-components/src/annotation.js
+++ packages/idyll-components/src/annotation.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import Image from './image.js';
+const { filterChildren } = require('idyll-component-children');
 
 class Annotation extends React.PureComponent {
   render() {
-    const hasGraphic = this.props.src;
+    const { children, annotation, ...props } = this.props;
     return (
       <span className="annotated-text">
-        {this.props.children}
+        {this.props.annotation}
         <div className="annotation-text">
-          <Image src={hasGraphic} />
-          {this.props.annotation}
+          {filterChildren(children, c => {
+            return c;
+          })}
         </div>
       </span>
     );
@@ -20,14 +21,10 @@ Annotation._idyll = {
   name: 'Annotation',
   tagType: 'open',
   displayType: 'inline',
-  children: [
-    'Content placed inside of an annotation component will be displayed inline with some styling.'
-  ],
   props: [
     {
       name: 'annotation',
       type: 'string',
-      src: 'src',
       example: '"This is annotation text"',
       description: 'The displayed text when user hovers on annotated text.'
     }
