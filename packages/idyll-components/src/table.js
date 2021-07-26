bf1aab77978beeb0c1f5a20d302e8444ae077743
Megan V <meganv@Megans-MacBook-Pro-2.local>
2021-07-25 18:33:31 -0700

    update react table to 7.7 and migrate

--- packages/idyll-components/src/table.js
+++ packages/idyll-components/src/table.js
@@ -1,57 +1,138 @@
 const React = require('react');
-const Table = require('react-table').default;
+import { useTable, useSortBy } from 'react-table';
 
-class TableComponent extends React.PureComponent {
-  getColumns() {
-    if (this.props.columns) {
-      if (
-        this.props.columns.length &&
-        typeof this.props.columns[0] === 'string'
-      ) {
-        return this.props.columns.map(d => {
-          return {
-            Header: d,
-            accessor: d
-          };
-        });
+/**
+ * Props: data (arr of objects), defaultPageSize (num), showPagination (bool),
+ *        showPageSizeOptions (bool), showPageJump (bool), className, value
+ *
+ *  display
+ *  sorting
+ *  pagination
+ */
+const TableComponent = props => {
+  if (!props.data && props.value) {
+    props.data = props.value;
+  }
+
+  // create data and columns
+  const data = React.useMemo(() => props.data, [props.data]);
+  const columns = React.useMemo(
+    () => {
+      if ((props.data || []).length) {
+        return Object.keys(props.data[0])
+          .filter(d => d !== '')
+          .map(d => {
+            return {
+              Header: d,
+              accessor: d
+            };
+          });
       }
+      return [];
+    },
+    [props.data]
+  );
 
-      return this.props.columns;
-    }
-    if ((this.props.value || this.props.data || []).length) {
-      return Object.keys((this.props.value || this.props.data)[0])
-        .filter(d => d !== '')
-        .map(d => {
-          return {
-            Header: d,
-            accessor: d
-          };
-        });
-    }
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({ columns, data }, useSortBy);
 
-    return [];
-  }
-  render() {
-    let { idyll, hasError, updateProps, ...props } = this.props;
-    if (!props.data && props.value) {
-      props.data = props.value;
-    }
-    return (
-      <Table
-        className={`table ${props.className || ''}`}
-        showPagination={props.data.length > props.defaultPageSize}
-        minRows={
-          props.data.length <= props.defaultPageSize
-            ? props.data.length
-            : undefined
-        }
-        {...props}
-        children={undefined}
-        columns={this.getColumns()}
-      />
-    );
-  }
-}
+  return (
+    <table {...getTableProps()} className={`table ${props.className || ''}`}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => {
+              const sortStyle = column.isSorted
+                ? !column.isSortedDesc
+                  ? {
+                      borderTop: '2px solid black'
+                    }
+                  : { borderBottom: '2px solid black' }
+                : {};
+              return (
+                <th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  style={{ ...sortStyle, cursor: 'pointer' }}
+                >
+                  {column.render('Header')}
+                </th>
+              );
+            })}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          // must call this every render
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+// class TableComponent extends React.PureComponent {
+// getColumns() {
+//   if (this.props.columns) {
+//     if (
+//       this.props.columns.length &&
+//       typeof this.props.columns[0] === 'string'
+//     ) {
+//       return this.props.columns.map(d => {
+//         return {
+//           Header: d,
+//           accessor: d
+//         };
+//       });
+//     }
+
+//     return this.props.columns;
+//   }
+// if ((this.props.value || this.props.data || []).length) {
+//   return Object.keys((this.props.value || this.props.data)[0])
+//     .filter(d => d !== '')
+//     .map(d => {
+//       return {
+//         Header: d,
+//         accessor: d
+//       };
+//     });
+//   }
+
+//   return [];
+// }
+//   render() {
+//     let { idyll, hasError, updateProps, ...props } = this.props;
+//     if (!props.data && props.value) {
+//       props.data = props.value;
+//     }
+//     return (
+//       <Table
+//         className={`table ${props.className || ''}`}
+//         showPagination={props.data.length > props.defaultPageSize}
+//         minRows={
+//           props.data.length <= props.defaultPageSize
+//             ? props.data.length
+//             : undefined
+//         }
+//         {...props}
+//         children={undefined}
+//         columns={this.getColumns()}
+//       />
+//     );
+//   }
+// }
 
 TableComponent.defaultProps = {
   data: [],
