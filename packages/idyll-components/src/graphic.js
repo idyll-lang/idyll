4130a95ba93056be33f9a924f284d217fdf8b499
Matthew Conlen <mc@mathisonian.com>
2021-10-15 13:01:19 -0700

    update graphic classname (#751)

--- packages/idyll-components/src/graphic.js
+++ packages/idyll-components/src/graphic.js
@@ -3,15 +3,13 @@ const React = require('react');
 class Graphic extends React.Component {
   render() {
     const { idyll, updateProps, hasError, ...props } = this.props;
-    return (
-      <div {...props} />
-    );
+    return <div className="idyll-graphic" {...props} />;
   }
 }
 
 Graphic._idyll = {
-  name: "Graphic",
-  tagType: "open"
-}
+  name: 'Graphic',
+  tagType: 'open'
+};
 
 module.exports = Graphic;
