2ec987ab2860783b91b61ab4be281c36955fc2af
jheer <jeffrey.heer@gmail.com>
2022-04-04 13:41:01 -0700

    feat: Allow pre-parsed AST to be passed as option.

--- packages/idyll-cli/src/pipeline/index.js
+++ packages/idyll-cli/src/pipeline/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
-const Promise = require('bluebird');
-const writeFile = Promise.promisify(fs.writeFile);
+const { writeFile } = fs.promises;
 const { copy, pathExists } = require('fs-extra');
+const { isVariableNode } = require('idyll-ast');
 const compile = require('idyll-compiler');
 const Terser = require('terser');
 const { paramCase } = require('change-case');
@@ -20,7 +20,7 @@ const errors = require('../errors');
 
 let output;
 
-const build = (opts, paths, resolvers) => {
+const build = async (opts, paths, resolvers) => {
   // always store source in opts.inputString
   if (paths.IDYLL_INPUT_FILE) {
     try {
@@ -30,115 +30,107 @@ const build = (opts, paths, resolvers) => {
     }
   }
 
-  const context = {
-    ...opts.compiler,
-    paths,
-    alias: opts.alias
+  const ast =
+    opts.ast ||
+    (await compile(opts.inputString, {
+      ...opts.compiler,
+      paths,
+      alias: opts.alias
+    }));
+
+  const template = fs.readFileSync(paths.HTML_TEMPLATE_FILE, 'utf8');
+
+  /* Change here */
+  resolvers.set('components', new ComponentResolver(opts, paths));
+
+  let nameArray = [];
+  getComponentNodes(ast).forEach(node => {
+    if (isVariableNode(node)) {
+      nameArray.push(node.type);
+    } else {
+      // console.log('checking component names - ', node);
+      nameArray.push(node.name.split('.')[0]);
+    }
+  });
+  const uniqueComponents = Array.from(new Set(nameArray));
+  const components = uniqueComponents.reduce((acc, name) => {
+    let resolved = resolvers.get('components').resolve(name);
+    if (resolved) acc[paramCase(name)] = resolved;
+    return acc;
+  }, {});
+
+  const data = getDataNodes(ast).reduce((acc, { name, source, async }) => {
+    let { resolvedName, data } = resolvers
+      .get('data')
+      .resolve(name, source, async);
+    acc[resolvedName] = data;
+    return acc;
+  }, {});
+
+  const css = resolvers.get('css').resolve();
+
+  output = {
+    ast: ast,
+    components,
+    data,
+    css,
+    syntaxHighlighting: getHighlightJS(ast, paths),
+    context: opts.context,
+    opts: {
+      ssr: opts.ssr,
+      theme: opts.theme,
+      layout: opts.layout,
+      authorView: opts.authorView
+    }
   };
-  return compile(opts.inputString, context)
-    .then(ast => {
-      return Promise.try(() => {
-        const template = fs.readFileSync(paths.HTML_TEMPLATE_FILE, 'utf8');
+  if (!opts.ssr) {
+    output.html = getBaseHTML(ast, template, opts);
+  } else {
+    output.html = getHTML(
+      paths,
+      ast,
+      output.components,
+      output.data,
+      template,
+      opts
+    );
+  }
 
-        /* Change here */
-        resolvers.set('components', new ComponentResolver(opts, paths));
+  let js = await bundleJS(opts, paths, output); // create index.js bundle
 
-        let nameArray = [];
-        getComponentNodes(ast).forEach(node => {
-          if (['var', 'derived', 'data'].indexOf(node.type) > -1) {
-            nameArray.push(node.type);
-          } else {
-            // console.log('checking component names - ', node);
-            nameArray.push(node.name.split('.')[0]);
-          }
-        });
-        const uniqueComponents = Array.from(new Set(nameArray));
-        const components = uniqueComponents.reduce((acc, name) => {
-          let resolved = resolvers.get('components').resolve(name);
-          if (resolved) acc[paramCase(name)] = resolved;
-          return acc;
-        }, {});
+  // minify bundle if necessary and store it
+  if (opts.minify) {
+    js = Terser.minify(js, {
+      mangle: { keep_fnames: true }
+    }).code;
+  }
+  output.js = js;
 
-        const data = getDataNodes(ast).reduce(
-          (acc, { name, source, async }) => {
-            let { resolvedName, data } = resolvers
-              .get('data')
-              .resolve(name, source, async);
-            acc[resolvedName] = data;
-            return acc;
-          },
-          {}
-        );
-
-        const css = resolvers.get('css').resolve();
-
-        output = {
-          ast: ast,
-          components,
-          data,
-          css,
-          syntaxHighlighting: getHighlightJS(ast, paths),
-          context: opts.context,
-          opts: {
-            ssr: opts.ssr,
-            theme: opts.theme,
-            layout: opts.layout,
-            authorView: opts.authorView
-          }
-        };
-        if (!opts.ssr) {
-          output.html = getBaseHTML(ast, template, opts);
-        } else {
-          output.html = getHTML(
-            paths,
-            ast,
-            output.components,
-            output.data,
-            template,
-            opts
-          );
+  // write output
+  await Promise.all([
+    writeFile(paths.JS_OUTPUT_FILE, output.js),
+    writeFile(paths.CSS_OUTPUT_FILE, output.css),
+    writeFile(paths.HTML_OUTPUT_FILE, output.html),
+    pathExists(paths.STATIC_DIR)
+      .then(exists => {
+        if (exists && paths.STATIC_DIR !== paths.STATIC_OUTPUT_DIR) {
+          return copy(paths.STATIC_DIR, paths.STATIC_OUTPUT_DIR);
         }
-      });
-    })
-    .then(() => {
-      return bundleJS(opts, paths, output); // create index.js bundle
-    })
-    .then(js => {
-      // minify bundle if necessary and store it
-      if (opts.minify) {
-        js = Terser.minify(js, {
-          mangle: { keep_fnames: true }
-        }).code;
-      }
-      output.js = js;
-    })
-    .then(() => {
-      return Promise.all([
-        writeFile(paths.JS_OUTPUT_FILE, output.js),
-        writeFile(paths.CSS_OUTPUT_FILE, output.css),
-        writeFile(paths.HTML_OUTPUT_FILE, output.html),
-        pathExists(paths.STATIC_DIR)
-          .then(exists => {
-            if (exists && paths.STATIC_DIR !== paths.STATIC_OUTPUT_DIR) {
-              return copy(paths.STATIC_DIR, paths.STATIC_OUTPUT_DIR);
-            }
-            return null;
-          })
-          .catch(e => {
-            debug('Error copying static files', e);
-          })
-      ]);
-    })
-    .then(() => {
-      return output; // return all results
-    });
+        return null;
+      })
+      .catch(e => {
+        debug('Error copying static files', e);
+      })
+  ]);
+
+  // return all results
+  return output;
 };
 
-const updateCSS = (paths, css) => {
+const updateCSS = async (paths, css) => {
   output.css = css.resolve();
-  return writeFile(paths.CSS_OUTPUT_FILE, output.css).then(() => {
-    return output; // return all results
-  });
+  await writeFile(paths.CSS_OUTPUT_FILE, output.css);
+  return output; // return all results
 };
 
 module.exports = {
