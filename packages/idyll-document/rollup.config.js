b69bf4a778204a9a00fa62b908441bd7c28b2f22
Matthew Conlen <mc@mathisonian.com>
2022-04-08 12:50:57 -0700

    fix rollup warnings

--- packages/idyll-document/rollup.config.js
+++ packages/idyll-document/rollup.config.js
@@ -9,41 +9,68 @@ const dependencies = [
   ...Object.keys(pkg.peerDependencies || {})
 ];
 
-export default {
-  input: 'src/index.js',
-  external: [/@babel\/runtime/, /csv-parse\/sync/, ...dependencies],
-  onwarn: function(warning, warn) {
-    if (warning.code === 'EVAL') return;
-    warn(warning);
-  },
-  output: [
-    {
-      file: 'dist/cjs/index.js',
-      format: 'cjs',
-      sourcemap: true,
-      name: 'idyll-document',
-      exports: 'auto'
+export default [
+  {
+    input: 'src/index.js',
+    external: [/@babel\/runtime/, /csv-parse\/sync/, ...dependencies],
+    onwarn: function(warning, warn) {
+      if (warning.code === 'EVAL') return;
+      warn(warning);
     },
-    {
-      file: 'dist/esm/index.mjs',
-      format: 'esm',
-      sourcemap: true,
-      exports: 'auto',
-      plugins: [
-        replace({
+    output: [
+      {
+        file: 'dist/cjs/index.js',
+        format: 'cjs',
+        sourcemap: true,
+        name: 'idyll-document',
+        exports: 'auto'
+      }
+    ],
+    plugins: [
+      babel({
+        babelHelpers: 'runtime',
+        exclude: 'node_modules/**' // only transpile our source code
+      }),
+      // jsx( { factory: "React.createElement" } ),
+      // nodeResolve(),
+      commonjs(),
+      builtins()
+    ]
+  },
+  {
+    input: 'src/index.js',
+    external: [
+      /@babel\/runtime/,
+      /csv-parse\/browser\/esm\/sync/,
+      ...dependencies
+    ],
+    onwarn: function(warning, warn) {
+      if (warning.code === 'EVAL') return;
+      warn(warning);
+    },
+    output: [
+      {
+        file: 'dist/esm/index.mjs',
+        format: 'esm',
+        sourcemap: true,
+        exports: 'auto'
+      }
+    ],
+    plugins: [
+      babel({
+        babelHelpers: 'runtime',
+        exclude: 'node_modules/**' // only transpile our source code
+      }),
+      replace({
+        values: {
           'csv-parse/sync': 'csv-parse/browser/esm/sync'
-        })
-      ]
-    }
-  ],
-  plugins: [
-    babel({
-      babelHelpers: 'runtime',
-      exclude: 'node_modules/**' // only transpile our source code
-    }),
-    // jsx( { factory: "React.createElement" } ),
-    // nodeResolve(),
-    commonjs(),
-    builtins()
-  ]
-};
+        },
+        preventAssignment: true
+      }),
+      // jsx( { factory: "React.createElement" } ),
+      // nodeResolve(),
+      commonjs(),
+      builtins()
+    ]
+  }
+];
